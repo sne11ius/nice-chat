cf33da52fda145bd9927452372b608310f9fe81a
Cornelius Wichering <cornelius.wichering@gmail.com>
2025-04-02 21:03:25 +0200

    Add username

--- src/main/java/network/discordia/ChatSocket.java
+++ src/main/java/network/discordia/ChatSocket.java
@@ -6,50 +6,51 @@ import jakarta.websocket.OnError;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/chat")
+@ServerEndpoint("/chat/{username}")
 @ApplicationScoped
 public class ChatSocket
 {
 
-    List<Session> sessions = new CopyOnWriteArrayList<>();
+    Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session)
+    public void onOpen(Session session, @PathParam("username") String username)
     {
-        broadcast("New user joined, say hello!");
-        sessions.add(session);
+        broadcast("New user " + username + " joined, say hello!");
+        sessions.put(username, session);
     }
 
     @OnClose
-    public void onClose(Session session)
+    public void onClose(Session session, @PathParam("username") String username)
     {
-        sessions.remove(session);
-        broadcast("User left");
+        sessions.remove(username);
+        broadcast("User " + username + " left");
     }
 
     @OnError
-    public void onError(Session session, Throwable throwable)
+    public void onError(Session session, @PathParam("username") String username, Throwable throwable)
     {
         sessions.remove(session);
-        broadcast("User left on error: " + throwable);
+        broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
-    public void conMessage(String message)
+    public void conMessage(String message, @PathParam("username") String username)
     {
-        broadcast(">> " + message);
+        broadcast(">> " + username + ": " + message);
     }
 
     private void broadcast(String message)
     {
-        sessions.forEach(s ->
+        sessions.values().forEach(session ->
         {
-            s.getAsyncRemote().sendObject(message, result ->
+            session.getAsyncRemote().sendObject(message, result ->
             {
                 if (result.getException() != null)
                 {
